328b0b34d06b8e39015fe2edcad88f06c1fe8322
FadhilAmad <fadhilsyawaludin5@gmail.com>
2026-03-12 06:29:26 +0700

    perbaikan page form dan styling

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,27 +1,48 @@
-export default function Home() {
+import react from "react";
+
+function page() {
     return (
         <div>
-            <article class="border-2 border-black bg-white shadow-[4px_4px_0_0,8px_8px_0_0]">
-                <div class="bg-yellow-300 p-3">
-                    <div class="flex items-center justify-between">
-                        <strong class="text-xs/none font-bold uppercase">System Message</strong>
-
-                        <div class="flex gap-1">
-                            <div class="size-3 border-2 border-black bg-white"></div>
-                            <div class="size-3 border-2 border-black bg-white"></div>
-                        </div>
+            <form className="w-full max-w-sm">
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <htmlFor className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Full Name
+                        </htmlFor>
+                    </div>
+                    <div className="md:w-2/3">
+                        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe"/>
                     </div>
                 </div>
-
-                <div class="border-t-2 border-black p-4 sm:p-6">
-                    <h3 class="text-lg font-semibold text-black">Retro Window</h3>
-
-                    <p class="mt-2 text-sm text-pretty text-black">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nihil, sit quod quos
-                        quibusdam quam ducimus dolore necessitatibus delectus perspiciatis.
-                    </p>
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <htmlFor className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                            Password
+                        </htmlFor>
+                    </div>
+                    <div className="md:w-2/3">
+                        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************"/>
+                    </div>
                 </div>
-            </article>
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3"></div>
+                    <htmlFor className="md:w-2/3 block text-gray-500 font-bold">
+                        <input className="mr-2 leading-tight" type="checkbox"/>
+                            <span className="text-sm">
+                                Send me your newsletter!
+                            </span>
+                    </htmlFor>
+                </div>
+                <div className="md:flex md:items-center">
+                    <div className="md:w-1/3"></div>
+                    <div className="md:w-2/3">
+                        <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
-    );
+    )
 }
+export default page;
